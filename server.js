7182131d6e41c0e28d451f65a4fd42ac3c194463
George, B. Smith <georgebsmith.tech@gmail.com>
2022-02-06 12:13:36 +0100

    fixed a bug

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
+
+const io = (exports.io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
